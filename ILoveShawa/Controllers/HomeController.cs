2b622661db7a42ed934205146f3953fc9bb7bce2
MaxWellHays <MaxWellHays.ru@gmail.com>
2015-11-05 02:30:00 +0300

    Added first viewing for test shawashops and inserted a map to page Changed bootstrap version

--- ILoveShawa/Controllers/HomeController.cs
+++ ILoveShawa/Controllers/HomeController.cs
@@ -1,19 +1,47 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using ILoveShawa.Domain.IRepository;
+using System.Device.Location;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Web.UI;
 
 namespace ILoveShawa.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IShawaShopRepository shawaShopRepository;
+        public class _ShawaShop
+        {
+            public readonly int Id;
+            public readonly string Name;
+            public readonly GeoCoordinate Coord;
+
+            private static int maxId;
+
+            public _ShawaShop(string name, GeoCoordinate coord)
+            {
+                Id = maxId++;
+                Name = name;
+                Coord = coord;
+            }
+        }
+
+        private readonly List<_ShawaShop> testShawaShops;
+
         public HomeController(IShawaShopRepository shawaShopRepository)
         {
             this.shawaShopRepository = shawaShopRepository;
+            testShawaShops = new List<_ShawaShop>();
+            testShawaShops.AddRange(Enumerable.Range(0, 20).Select(
+                i => new _ShawaShop($"Шавашоп {i}", new GeoCoordinate(30.41, 59.90))));
         }
 
         public ActionResult Index()
         {
-            return View(shawaShopRepository.GetShawaShops());
+            var showindShops = testShawaShops.Take(10);
+            return View(showindShops);
         }
 
         public ActionResult About()
---
+++ ILoveShawa/Scripts/site.js
@@ -0,0 +1,10 @@
+﻿ymaps.ready(initMap);
+var myMap;
+
+function initMap() {
+    myMap = new ymaps.Map("index-map", {
+        center: [59.92095082, 30.30707118],
+        zoom: 11,
+        controls: []
+    });
+}
